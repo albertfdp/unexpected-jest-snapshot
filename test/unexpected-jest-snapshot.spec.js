f4167fbb288120888e2913e7abc24ff6409d9cdf
Albert Fernández <albert@nandez.cat>
2017-01-21 14:34:14 +0100

    Add a couple of tests

--- test/unexpected-jest-snapshot.spec.js
+++ test/unexpected-jest-snapshot.spec.js
@@ -1,19 +1,29 @@
 const unexpected = require('unexpected')
 const unexpectedJestSnapshot = require('../src')
 
+const React = require('react')
+const TestRenderer = require('react-test-renderer')
+
 const expect = unexpected.clone()
   .use(unexpectedJestSnapshot)
+
+const Component = ({ name }) => (
+  React.createElement('div', null, `Name: ${name}`)
+)
 
 describe('unexpected-jest-snapshot', () => {
   describe('snapshots', () => {
     it('creates a snapshot', () => {
-      return expect('foo', 'to match snapshot')
-        .then(() => {
-          expect(
-            () => expect('bar', 'to match snapshot'),
-            'to throw'
-          )
-        })
+      expect('foo', 'to match snapshot')
+    })
+  })
+
+  describe('with react-test-renderer', () => {
+    it('creates a snapshot of the rendered component', () => {
+      expect(
+        React.createElement(Component, { name: 'foo' }),
+        'to match snapshot'
+      )
     })
   })
 })
